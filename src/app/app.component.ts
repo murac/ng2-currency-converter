c8c94d99c08b446a9d6e90073b5b1b3a988d992a
Darek Dziamalek <ddziamalek@gmail.com>
2016-07-02 15:58:43 -0400

    no

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,18 @@
 import {Component} from '@angular/core';
 
 @Component({
-    selector: 'my-app',
-    template: '<h1>My App baby!</h1>'
+    selector: 'currency-converter',
+    template: `
+        <input type="number" value="{{baseAmount}}">USD = <strong>{{targetAmount}}</strong>GBP
+`,
+    styles: [`
+        input[type=number]{
+        width:10ex;
+        text-align: right;
+        }
+`]
 })
-export class AppComponent {}
+export class AppComponent {
+    baseAmount = 1;
+    targetAmount = 0.70;
+}
