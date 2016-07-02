318261b14bea86d66960bb5d2530062d0f1c4bb6
Darek Dziamalek <ddziamalek@gmail.com>
2016-07-02 16:55:45 -0400

    added service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,41 @@
 import {Component} from '@angular/core';
+import {ExchangeService} from "./exchange.service";
 
 @Component({
     selector: 'currency-converter',
     template: `
-        <input type="number" [value]="baseAmount" (input)="update($event.target.value)">
-        USD = <strong>{{targetAmount}}</strong>GBP
+        <h1>Currency Converter</h1>
+        Convert: <input type="number" [(ngModel)]="baseAmount" [ngClass]="{error: isInvalid(baseAmount)}">
+        <p>
+        <strong>{{baseAmount}}</strong> {{baseCurrency}} = <strong>{{targetAmount}}</strong>{{targetCurrency}}
+        </p>
 `,
     styles: [`
         input[type=number]{
         width:10ex;
         text-align: right;
         }
-`]
+        .error{
+        background-color:#ff6666;
+        }
+`],
+    providers: [ExchangeService]
 })
 export class AppComponent {
+    baseCurrency = "USD";
+    targetCurrency = "GBP";
     baseAmount = 1;
-    exchangeRate = 0.70;
-    targetAmount = this.exchangeRate;
 
-    update(baseAmount){
-        this.targetAmount = parseFloat(baseAmount) * this.exchangeRate;
+    constructor(private _exchangeService:ExchangeService) {
+    }
+
+    get targetAmount() {
+        const exchangeRate = this._exchangeService
+            .getExchangeRate(this.baseCurrency, this.targetCurrency);
+        return this.baseAmount * exchangeRate;
+    }
+
+    isInvalid(value) {
+        return !Number.isFinite(value);
     }
 }
