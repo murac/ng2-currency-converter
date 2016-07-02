42ba36d1e4b672d49a601cdd6773e00de699a31e
Darek Dziamalek <ddziamalek@gmail.com>
2016-07-02 16:16:17 -0400

    prelim

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,8 @@ import {Component} from '@angular/core';
 @Component({
     selector: 'currency-converter',
     template: `
-        <input type="number" value="{{baseAmount}}">USD = <strong>{{targetAmount}}</strong>GBP
+        <input type="number" [value]="baseAmount" (input)="update($event.target.value)">
+        USD = <strong>{{targetAmount}}</strong>GBP
 `,
     styles: [`
         input[type=number]{
@@ -14,5 +15,10 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
     baseAmount = 1;
-    targetAmount = 0.70;
+    exchangeRate = 0.70;
+    targetAmount = this.exchangeRate;
+
+    update(baseAmount){
+        this.targetAmount = parseFloat(baseAmount) * this.exchangeRate;
+    }
 }
